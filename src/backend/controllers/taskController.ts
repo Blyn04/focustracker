b429c079b1e4890d9ab4185460574a1216c497cb
berlene <berlenebernabe12@gmail.com>
2025-11-10 18:50:35 +0800

    TASKCONTROLLER UPDATE

--- src/backend/controllers/taskController.ts
+++ src/backend/controllers/taskController.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from "express";
-import Task, { ITask } from "../models/Task";
+import Task from "../models/Task";
 
 // Get all tasks
 export const getTasks = async (req: Request, res: Response) => {
   try {
-    const tasks: ITask[] = await Task.find().sort({ createdAt: -1 });
+    const tasks = await Task.find().sort({ createdAt: -1 }); // let TS infer type
     res.json(tasks);
   } catch (err) {
+    console.error(err);
     res.status(500).json({ error: "Server error" });
   }
 };
@@ -18,6 +19,7 @@ export const createTask = async (req: Request, res: Response) => {
     await task.save();
     res.status(201).json(task);
   } catch (err) {
+    console.error(err);
     res.status(500).json({ error: "Server error" });
   }
 };
@@ -28,6 +30,7 @@ export const deleteTask = async (req: Request, res: Response) => {
     await Task.findByIdAndDelete(id);
     res.json({ message: "Task deleted" });
   } catch (err) {
+    console.error(err);
     res.status(500).json({ error: "Server error" });
   }
 };
